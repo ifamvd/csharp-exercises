fe7fd7744958caad1e7ed9bc888a57c44ac65b16
adiputra.ismail <adiputra.ismail@gmail.com>
2018-11-01 19:00:55 -0500

    Finished the studio, created Area.

---
+++ Area/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14;
+            Console.Write("Enter a radius: ");
+            double r = 0.0;
+            double.TryParse(Console.ReadLine(), out r);
+            double area = pi * r * r;
+            Console.WriteLine("The area of a circle with radius " + r + " is: " + area);
+            Console.ReadLine();
+        }
+    }
+}
